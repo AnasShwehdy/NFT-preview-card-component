1374e769fd88b78a216c16a36b4638df45d1e843
AnasShwehdy <anas.jalal.ly@gmail.com>
2022-11-04 22:47:00 +0200

    fix: app title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+   app:{
+    head:{
+        title: 'NFT Preview Card'
+    }
+   },
     css: [
         'vuetify/lib/styles/main.sass',
         '@mdi/font/css/materialdesignicons.min.css',
